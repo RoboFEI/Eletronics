d536f7d84823c24ea7b72a6f835df6d4c5457687
Thiago M <rpj134@gmail.com>
2024-09-18 17:08:29 -0300

    nda

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ sudo add-apt-repository ppa:kicad/kicad-8.0-releases
 sudo apt update -y
 sudo apt install kicad -y
 
-cd Comm_Board/communication_board
+cd ~/Eletronics/Comm_Board/communication_board
 ln -s ~/Eletronics/Libraries Libraries
