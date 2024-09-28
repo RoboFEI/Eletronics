d6132fb92a370a55e5e5b90c3cfc5b698015807d
Thiago M <rpj134@gmail.com>
2024-09-28 15:38:18 -0300

    update Libraries and Power Board

--- setup.sh
+++ setup.sh
@@ -4,5 +4,10 @@ sudo add-apt-repository ppa:kicad/kicad-8.0-releases
 sudo apt update -y
 sudo apt install kicad -y
 
+rm -rf ~/Eletronics/Comm_Board/communication_board/Libraries 
 cd ~/Eletronics/Comm_Board/communication_board
+ln -s ~/Eletronics/Libraries Libraries
+
+rm -rf ~/Eletronics/Power_Board/Power_Board/Libraries 
+cd ~/Eletronics/Power_Board/Power_Board/
 ln -s ~/Eletronics/Libraries Libraries
